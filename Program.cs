3593dce8a777b4aa401ff2000b7edd69ab839178
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-05-28 22:39:48 +0300

    temel exception yapısı

--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
+using ExceptionHandlerAndSerilog.WebAPI.Services;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<UserService>();
+
+builder.Services.AddOpenApi();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.MapGet("/", (int age,UserService user) =>
+{
+    //if (age < 18)
+    //{
+    //    return Results.BadRequest(new
+    //    {
+    //        Message = "Kullanıcnın yaşı 18 den büyük olmalı"
+    //    });
+    //}
+    var result = user.Create(age);
+    return result.Item1 ? Results.Ok() : Results.BadRequest(result.Item2);
+});
+
 
 app.Run();
---
+++ Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿namespace ExceptionHandlerAndSerilog.WebAPI.Services;
+
+public sealed class UserService
+{
+    public (bool, string) Create(int age)
+    {
+        try
+        {
+            if (age < 18)
+            {
+                int a = age;
+                int b = 0;
+                int c = a / b;
+                return (false, "Kullanıcnın yaşı 18 den büyük olmalı");
+                //throw new Exception("Kullanıcnın yaşı 18 den büyük olmalı");
+            }
+            return (true, string.Empty);
+        }
+        catch (Exception ex)
+        {
+
+            return (false, ex.Message);
+        }
+
+    }
+}
